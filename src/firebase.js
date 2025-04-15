ba5277297fd2754340370e78299f2eec64983448
MoonWalka <131055109+MoonWalka@users.noreply.github.com>
2025-04-15 22:07:55 +0000

    tentative de resolution de problemme avec env

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+// Débogage des variables d'environnement
+console.log('REACT_APP_FIREBASE_API_KEY:', process.env.REACT_APP_FIREBASE_API_KEY ? 'définie' : 'non définie');
+console.log('REACT_APP_FIREBASE_AUTH_DOMAIN:', process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ? 'définie' : 'non définie');
+// etc.
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";

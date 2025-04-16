8082f336d04010e853be30c3c763d801df3927de
MoonWalka <131055109+MoonWalka@users.noreply.github.com>
2025-04-16 00:55:30 +0000

    artist page

--- src/pages/ArtistesPage.jsx
+++ src/pages/ArtistesPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import ArtistesList from '../components/artistes/ArtistesList';
 import ArtisteForm from '../components/artistes/ArtisteForm';
 import ArtisteDetail from '../components/artistes/ArtisteDetail';
@@ -12,6 +12,7 @@ function ArtistesPage() {
         <Route path="/nouveau" element={<ArtisteForm />} />
         <Route path="/:id" element={<ArtisteDetail />} />
         <Route path="/:id/modifier" element={<ArtisteForm />} />
+        {/* Route de secours pour rediriger les chemins inconnus */}
         <Route path="*" element={<Navigate to="/artistes" replace />} />
       </Routes>
     </div>

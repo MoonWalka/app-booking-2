4a988dc265178cf8ccddb9cd3c5102fb4dcb1773
Moon Walka <meltinrecordz@Mac-Studio-de-Moon.local>
2025-05-23 13:05:51 +0200

    🧹 Suppression du dossier de backup: refactorisation_backup_20250516_170903

--- refactorisation_backup_20250516_170903/src/components/ui/Badge.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import styles from './Badge.module.css';
-
-const Badge = (props) => {
-  return <div className={styles.container}>Badge Placeholder</div>;
-};
-
-export default Badge;

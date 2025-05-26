4c80977fca400fa4e01c7e1d9c7468faae436d33
Moon Walka <meltinrecordz@Mac-Studio-de-Moon.local>
2025-05-26 05:04:55 +0200

    Sauvegarde avant passage en stub de tous les modules sauf Concerts

--- src/pages/ArtistesPage.js
+++ src/pages/ArtistesPage.js
@@ -15,14 +15,14 @@ function ArtistesPage() {
       <Routes>
         <Route path="/" element={<ArtistesList />} />
         <Route path="/nouveau" element={<ArtisteForm />} />
-        <Route path="/:id" element={<ArtisteDetail />} />
-        <Route path="/:id/modifier" element={<ArtisteForm />} />
+        <Route path=":id" element={<ArtisteDetail />} />
+        <Route path=":id/modifier" element={<ArtisteForm />} />
         {/* 
         Pour adopter la structure de la branche refacto-structure-scriptshell - pour implémentation future
         <Route path="/" element={<ArtistesList />} />
         <Route path="/nouveau" element={<ArtisteForm />} />
-        <Route path="/:id" element={<ArtisteDetails />} /> 
-        <Route path="/:id/modifier" element={<ArtisteForm />} />
+        <Route path=":id" element={<ArtisteDetails />} /> 
+        <Route path=":id/modifier" element={<ArtisteForm />} />
         <Route path="*" element={<Navigate to="/artistes" replace />} />
         */}
         {/* Note: La principale différence est l'utilisation de ArtisteDetails au lieu de ArtisteDetail
